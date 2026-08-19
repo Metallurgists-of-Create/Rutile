8f6b9ee18fc304538bc9536a8dc13221a1121ed8
PouffyDev <99536749+pouffy@users.noreply.github.com>
2026-08-19 16:01:33 +0100

    fix mod loading

--- src/main/java/dev/metallurgists/rutile/Rutile.java
+++ src/main/java/dev/metallurgists/rutile/Rutile.java
@@ -1,37 +1,24 @@
 package dev.metallurgists.rutile;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.fml.loading.FMLEnvironment;
-import net.neoforged.fml.loading.FMLPaths;
-import org.slf4j.Logger;
-
-import java.nio.file.Path;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Contract;
 
 
 @Mod(Rutile.ID)
 public class Rutile {
-    private static Rutile INSTANCE;
     public static final String ID = "rutile";
     public static final String DISPLAY_NAME = "Rutile";
-    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
 
-    private final IEventBus modEventBus;
 
-    public Rutile(IEventBus modEventBus) {
-        this.modEventBus = modEventBus;
-        INSTANCE = this;
-
-        Rutile.init();
-    }
-
-    public static void init() {
-        LOGGER.info("{} is initializing...", DISPLAY_NAME);
+    public Rutile(IEventBus modEventBus, ModContainer modContainer) {
     }
 
     @SubscribeEvent
@@ -39,27 +26,12 @@ public class Rutile {
 
     }
 
+    @Contract("_ -> new")
     public static ResourceLocation id(String path) {
         if (path.contains(":")) {
-            return ResourceLocation.parse(path);
+            return ResourceLocation.tryParse(path);
         }
         return ResourceLocation.fromNamespaceAndPath(ID, path);
     }
 
-
-    public static IEventBus getEventBus() {
-        return INSTANCE.modEventBus;
-    }
-
-    public static Path getGameDir() {
-        return FMLPaths.GAMEDIR.get();
-    }
-
-    public static boolean isClientThread() {
-        return isClientSide() && Minecraft.getInstance().isSameThread();
-    }
-
-    public static boolean isClientSide() {
-        return FMLEnvironment.dist.isClient();
-    }
 }
